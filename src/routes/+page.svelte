d811d0083358be3eb38a25ca51031cd88f2e9b68
William Viktorsson <williamviktorsson@gmail.com>
2024-10-15 13:32:12 +0200

    reference

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { enhance } from "$app/forms";
+
+  export let data;
+</script>
+
+<h1>WELCOME {data.user.username} TO THE HOME PAGE</h1>
+
+<form use:enhance action="/login?/outlog" method="post">
+  <button>LOGOUT</button>
+</form>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { enhance } from "$app/forms";
+  import type { ActionData, PageData } from "./$types";
+
+  export let data: PageData;
+  export let form: ActionData;
+</script>
+
+<h1>LOGIN PAGE</h1>
+
+<form use:enhance action="?/login" method="post">
+  <div>
+    <input type="text" name="username" id="" />
+    <input type="password" name="password" id="" />
+    <button>LOGIN</button>
+  </div>
+  {#if form?.login_fail}
+    <span>{form.login_fail}</span>
+  {/if}
+</form>
+
+<style>
+  span {
+    color: red;
+  }
+</style>
